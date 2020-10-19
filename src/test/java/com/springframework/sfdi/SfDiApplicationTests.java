0518717f425e645bc0f8fb34e62130911863d070
Zenab Bohra <zenab.bohra19@gmail.com>
2020-10-19 15:03:11 +1100

    Create a simple controller to show dependency injection in Spring context

---
+++ src/test/java/com/springframework/sfdi/SfDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.sfdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
